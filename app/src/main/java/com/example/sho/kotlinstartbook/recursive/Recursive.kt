ae3c266190a62d5cd8d8254d4d0701d285fda88f
ShoUshikubo <sh-ushikubo@kddi.com>
2018-10-16 13:40:35 +0900

    フィボナッチを追加

---
+++ app/src/main/java/com/example/sho/kotlinstartbook/recursive/Recursive.kt
@@ -0,0 +1,18 @@
+package com.example.sho.kotlinstartbook.recursive
+
+/**
+ * TODO クラス説明
+ *
+ * Created by sho on 2018/10/16.
+ */
+class Recursive {
+
+    fun fibonacci(n: Int): Int {
+        return when(n) {
+            0 -> 0
+            1 -> 1
+            2 -> 1
+            else -> fibonacci(n - 2) + fibonacci(n - 1)
+        }
+    }
+}
